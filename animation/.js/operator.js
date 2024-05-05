31ef5a93d707d372300a23d2d4a851313283aced
Kavali Maheshawri <kavalimaheswari77@gmail.com>
2024-05-05 11:02:35 +0530

    update

---
+++ animation/.js/operator.js
@@ -0,0 +1,92 @@
+i=20
+{
+    console.log(i)
+}
+var a="maheswari"
+{
+    console.log(a)
+}
+
+function mahesawariPrint ()
+{
+    console.log("print mahesawari");
+}
+mahesawariPrint()
+for (var i=25; i>=1; i-=2)
+{
+    console.log(i)
+}
+var arr = ["welcome","everyone","to","js"]
+for (var a of arr)
+{
+    console.log(a)
+}
+for (var a in arr)
+{
+ console.log(a)
+}
+
+function bad()
+{
+    console.log("i'm bad")
+}
+function mad()
+{
+ console.log("i 'm  mad")
+}
+mad()
+function bad()
+{
+    console.log("i'm bad")
+}
+console.log(bad)
+function mad(a,someting)
+{
+ console.log(someting)
+ console.log("i'm mad")
+}
+mad(2,bad)
+var n=19
+while(n>=1)
+{
+    console.log(n)
+    n-=2
+}
+var s="maheswari"
+var n = s.length-1
+while(n>=0)
+{
+    console.log(s.charAt(n))
+    n--
+}
+var s='maheswari'
+var n=0
+var vow=0
+var cons=0
+while(n<=s.length-1)
+{
+    if(s.charAt(n))=='a'
+    vow++;
+    n++
+}
+console.log('vowels in maheswari are ${vow}')
+console.log('Consonants in maheswari are $(cons}')
+var results =cube(5);
+function area(a)
+{
+    console.log(Math.PI* a * a)
+}
+area(3)
+area(3,square)
+function area (val, fn)
+{
+    fn(val);
+}
+var x = function ()
+{
+    console.log("something");
+}
+(function()
+{
+    console.log("my LIFE")
+})()
